f68b73590836152f71e34c1dd38449889a4c2d78
Monster IT <alex@monster-it.co.uk>
2025-09-25 14:36:20 +0100

    change1111

---
+++ ServerScriptService/ToiletService.lua
@@ -0,0 +1,22 @@
+local CollectionService = game:GetService("CollectionService")  
+
+
+local Toliet = require(script.Toliet)
+
+
+-- @FIXME: abstract out an agnostic tag list
+local function makeToliet(tolietModel)
+    local _toliet = Toliet.new(tolietModel)
+
+    _toliet:_bind()
+end
+
+local _tolietList = CollectionService:GetTagged('Toliet')
+
+for i = 1, #_tolietList do
+    makeToliet(_tolietList[i])
+end
+
+CollectionService:GetInstanceAddedSignal('Toliet'):Connect(function(tolietModel)
+    makeToliet(tolietModel)
+end)
